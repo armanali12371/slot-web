f31e367ae2866fdaab3d2195c0540fb0304c8e1a
armanali12371 <armanali12371@gmail.com>
2024-12-19 01:02:46 +0530

    build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
-import type { NextConfig } from "next";
+module.exports = {
+  reactStrictMode: false,
+  swcMinify: true,
+  distDir: 'build_1',
+ 
+  //dev///////////
+  env: {
+    API_PORT: 'http://159.89.169.237:3000'
+  },
+  generateBuildId: async () => {
+    return '' + new Date().getTime();
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
+
 };
-
-export default nextConfig;
